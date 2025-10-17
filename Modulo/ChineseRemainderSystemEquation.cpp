51a6ea6f868fc432980ac77ae3b6cc89151a49d6
ak12abckhoi <ak12abckhoi@gmail.com>
2025-10-17 11:18:19 +0700

    Update ChineseRemainderSystemEquation.cpp

--- Modulo/ChineseRemainderSystemEquation.cpp
+++ Modulo/ChineseRemainderSystemEquation.cpp
@@ -1,17 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int powerMod(int a, int k, int n) {
-    int result = 1;
-    a = a % n;
-    while (k > 0) {
-        if (k % 2 == 1) result = (result * a) % n;
-        a = (a * a) % n;
-        k /= 2;
-    }
-    return result;
-}
-
 int extended_gcd(int a, int b, int &x, int &y) {
     if (a == 0) {
         x = 0;
@@ -39,44 +28,6 @@ int mod_inverse(int a, int m) {
     }
 }
 
-vector<int> prime_factors(int n) {
-    vector<int> factors;
-    for (int i = 2; i * i <= n; i++) {
-        while (n % i == 0) {
-            factors.push_back(i);
-            n /= i;
-        }
-    }
-    if (n > 1) factors.push_back(n);  
-    return factors;
-}
-
-int chineseRemainder(int a, int k, int n) {
-    vector<int> factors = prime_factors(n);
-    int M = 1;
-    vector<int> r, M_i, y;
-
-    for (int m_i : factors) {
-        int r_i = powerMod(a, k, m_i);
-        r.push_back(r_i);
-        M *= m_i;
-    }
-
-    for (int i = 0; i < factors.size(); i++) {
-        M_i.push_back(M / factors[i]);
-        y.push_back(mod_inverse(M_i[i], factors[i]));
-    }
-
-    int result = 0;
-    for (int i = 0; i < factors.size(); i++) {
-        result = (result + 1LL * r[i] * M_i[i] * y[i]) % M;
-    }
-
-    if (result < 0) result += M;
-
-    return result;
-}
-
 int chinese_remainder_theorem(int a1, int m1, int a2, int m2, int a3, int m3) {
 
     int M = m1 * m2 * m3;
@@ -101,4 +52,5 @@ int chinese_remainder_theorem(int a1, int m1, int a2, int m2, int a3, int m3) {
 int main(){
 	cout << "Output: x = " << chinese_remainder_theorem(17,19,11,5,16,3) << endl;
 }
+
 
