e5739eb387f5fc86697cfe18db56cd52dbdf08aa
Van Nang <vannang20050414@gmail.com>
2025-10-24 14:42:18 +0700

    bai5 ma hoa co dien

---
+++ Ma hoa co dien/bai5.py
@@ -0,0 +1,16 @@
+'''
+MẬT MÃ MA TRẬN KHÓA PLAYFAIR
+Input: M = THETRUTHWILLOU
+Key: K = THEDIEIS
+Tìm Output: C =
+'''
+import playfair
+
+M = "THETRUTHWILLOU"
+K = "THEDIEIS"
+
+p = playfair.Playfair(K)
+
+print(f"C = {p.__encrypt__(M)}")
+
+print(p.key)
