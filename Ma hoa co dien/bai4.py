e07fa4293556b4c908950675dda5f0a8615a2700
Van Nang <vannang20050414@gmail.com>
2025-10-24 14:09:17 +0700

    bai4 ma hoa co dien

---
+++ Ma hoa co dien/bai4.py
@@ -0,0 +1,13 @@
+'''
+MÃ HÓA CHỮ ĐƠN
+Input: M = AWOMANGIVESANDFO
+Key: K = THLEYNPSXADWKFUBOGMQVJRCIZ
+Tìm Output: C =
+'''
+
+import ma_hoa_chu_don as mhcd
+
+M = "AWOMANGIVESANDFO"
+K = "THLEYNPSXADWKFUBOGMQVJRCIZ"
+
+print(f"C = {mhcd.encrypt(M, K)}")
